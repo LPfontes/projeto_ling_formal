a6ab567447492acb31d5bfcd796c4d5d5d83f832
Luiz paulo de souza fontes junior <82241012+LPfontes@users.noreply.github.com>
2024-09-30 18:25:32 -0300

    Construção da AFD minimizada

--- tfm/views.py
+++ tfm/views.py
@@ -7,11 +7,16 @@ def estado_final_transicao(transicoes,estadoInicial,caracter):
         estadoInicialTransicao, estadoFinalTransicao, caracterTransicao = transicao.split(',')
         if estadoInicialTransicao == estadoInicial and caracterTransicao == caracter:
             return estadoFinalTransicao
-def validar_tupla(tupla,combinacoesDict,transicoes,valorTrasicao):
-
-    tuplaTrasicao = estado_final_transicao(transicoes,tupla[0],valorTrasicao),estado_final_transicao(transicoes,tupla[1],valorTrasicao)
-    if combinacoesDict.get(''.join(tuplaTrasicao),0):
-        combinacoesDict[''.join(tupla)] = True
+def validar_tupla(tuplasEstados,combinacoesDict,transicoes,alfabeto):
+    alteracao = False
+    for tupla in tuplasEstados: # marcamos os estados onde as transições de cada par levam a um novo par existente e já marcado
+        if not combinacoesDict.get(''.join(tupla)):
+            for caracter in alfabeto:
+                tuplaTrasicao = estado_final_transicao(transicoes,tupla[0],caracter),estado_final_transicao(transicoes,tupla[1],caracter)
+                if combinacoesDict.get(''.join(tuplaTrasicao), 0) or combinacoesDict.get(''.join(reversed(tuplaTrasicao)), 0):
+                    combinacoesDict[''.join(tupla)] = True
+                    alteracao = True
+    return alteracao
     
 def automato_view(request):
     if request.method == 'POST':
@@ -31,21 +36,15 @@ def automato_view(request):
             for tupla in tuplasEstados: # marcamos os estados onde apenas um dos pares é Estado final
                 if(tupla[0] in finais and not tupla[1] in finais or not tupla[0] in finais and tupla[1] in finais):
                     combinacoesDict[''.join(tupla)] = True
+            alteracao = True
+            while(alteracao):
+                alteracao = validar_tupla(tuplasEstados,combinacoesDict,transicoes,alfabeto)
             
-            for tupla in tuplasEstados: # marcamos os estados onde as transições de cada par levam a um novo par existente e já marcado
+            for tupla in tuplasEstados: 
                 if not combinacoesDict.get(''.join(tupla)):
-                    for caracter in alfabeto:
-                        validar_tupla(tupla,combinacoesDict,transicoes,caracter)
-            afdMinimizada = []
-            for tupla in tuplasEstados: # adicionamos a lista os estados que não são equivalentes
-                if combinacoesDict.get(''.join(tupla)):
-                    afdMinimizada.append(tupla[0])
-                    afdMinimizada.append(tupla[1])
-                else:
-                     uniaoEstados.append(tupla) # adicionamos a lista os estados são equivalentes e podem ser unidos
-            
+                    uniaoEstados.append(tupla) # adicionamos a lista os estados são equivalentes e podem ser unidos
+            repetidos = []
             for i, tupla1 in enumerate(uniaoEstados):
-                repetidos = []
                 for j in range(i, len(uniaoEstados)):  # j começa no índice i
                     tupla2 = uniaoEstados[j]
                     # Não comparar a tupla com ela mesma
@@ -53,7 +52,28 @@ def automato_view(request):
                         # Verifica se algum elemento de tupla1 está em tupla2
                         if any(elem in tupla2 for elem in tupla1):
                             repetidos.append((tupla1, tupla2))
-            
+
+            uniao = set()
+            # Juntar todas as tuplas de repetidos em um único conjunto
+            for par in repetidos:
+                # Adiciona os elementos de ambas as tuplas no conjunto
+                for tupla in par:
+                    uniao.update(tupla)   
+            remover = []
+            for tupla in uniaoEstados:
+                if any(elem in tupla for elem in uniao):
+                    remover.append(tupla)
+            for tupla in remover:
+                uniaoEstados.remove(tupla)
+            uniaoEstados.append(uniao)
+            for estado in estados:
+                if not any(estado in tupla for tupla in uniaoEstados):
+                    uniaoEstados.append(estado)
+            afdMinimizada = []
+            for element in uniaoEstados: 
+                afdMinimizada.append(''.join(element))
+
+                    
             context = {
                 'alfabeto': alfabeto,
                 'estados': estados,
@@ -61,6 +81,7 @@ def automato_view(request):
                 'finais': finais,
                 'transicoes': transicoes,
                 'combinacoesDict': combinacoesDict,  # Passando as combinações para o template
+                'afdMinimizada':afdMinimizada
             }
 
             return render(request, 'tfm\\automato_result.html', context)
